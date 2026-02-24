8a061d7cb2c452e2133317136a7f23d8d6669c1a
Tim Wetherill <wetherillt@gmail.com>
2026-02-24 09:50:11 -0700

    Add error logging + disable broken earnings lookup

--- py-engine/app/main.py
+++ py-engine/app/main.py
@@ -163,6 +163,8 @@ async def init_session(watchlist: list[str] = []):
         }
 
     except Exception as e:
+        import traceback
+        traceback.print_exc()
         raise HTTPException(status_code=500, detail=str(e))
 
 
